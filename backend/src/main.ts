b9c959575eb3a1f56f9be9a79f5c038f113e3d49
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2023-04-09 19:55:00 +0300

    Add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const start = async () => {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder().setTitle('Social network api').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 };
 
