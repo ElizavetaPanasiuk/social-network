70ab6ecc7f839416eafe1230c749b8b411f6b7ad
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2023-04-15 14:40:24 +0300

    Login layout

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,20 +1,33 @@
-import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
-import { Box, Input } from '@/ui-kit';
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { Box, Button, Input } from "@/ui-kit";
+import styles from "./styles.module.scss";
 
 const LoginPage = () => {
   const { t } = useTranslation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <div>
-      <Box>
-        <h1>{t('Login')}</h1>
-        <Input value={email} placeholder={t('Email') as string} onChange={setEmail} />
-        <Input value={password} placeholder={t('Password') as string} onChange={setPassword} />
-      </Box>
-    </div>
+    <Box className={styles.loginContainer}>
+      <h1>{t("Login")}</h1>
+      <Input
+        value={email}
+        placeholder={t("Email") as string}
+        onChange={setEmail}
+      />
+      <Input
+        value={password}
+        placeholder={t("Password") as string}
+        onChange={setPassword}
+      />
+      <Button
+        title={t("Sign In")}
+        onClick={() => {
+          console.log("Войти");
+        }}
+      />
+    </Box>
   );
 };
 
